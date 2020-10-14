8a98cce4f6dfe9b99846aabcf3fb7f11858a4ebc
Ryan Xin <xinpf0715@gmail.com>
2020-10-14 22:24:36 +1100

    accordion function done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log("JS loaded");
+window.onload = () => {
+  const listTitles = document.getElementsByClassName("list-title");
+  const arrows = document.getElementsByClassName("arrow");
+  const contents = document.getElementsByClassName("list-content");
+  console.log(listTitles);
+  console.log(arrows);
+  console.log(contents);
+
+  for (let i = 0; i < listTitles.length; i++) {
+    listTitles[i].addEventListener("click", (e) => {
+      
+      if (e.target.nextElementSibling.classList.contains("arrow-up")) {
+        e.target.nextElementSibling.classList.remove("arrow-up");
+        e.target.parentElement.nextElementSibling.classList.add("hide");
+      } else {
+        for (let j = 0; j < listTitles.length; j++) {
+          arrows[j].classList.remove("arrow-up");
+          contents[j].classList.add("hide");
+        }
+        e.target.nextElementSibling.classList.add("arrow-up");
+        e.target.parentElement.nextElementSibling.classList.remove("hide");
+      }
+
+    });
+  }
+}
